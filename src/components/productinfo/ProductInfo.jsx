ec1afcfa1a0b403544aab006f733fc49fe141b49
GabSalemi <salemi.gabriele94@gmail.com>
2023-06-29 17:53:04 +0200

    final commit

---
+++ src/components/productinfo/ProductInfo.jsx
@@ -0,0 +1,22 @@
+import "./index.css"
+
+const ProductInfo = ({data}) => {
+
+    console.log(data)
+    return <div className="fixed__card">
+             {data === "" ?  
+            <h2>We can't find any products matching your search</h2>
+           : <div className="product__info">
+                <h2>{data.name}</h2>
+                <h4>{data.name}</h4>
+                <p>{data.description}</p>
+                <img src={data.image_src} alt={data.name} />
+            </div>
+            }
+           
+           
+        </div>
+
+}
+
+export default ProductInfo;
