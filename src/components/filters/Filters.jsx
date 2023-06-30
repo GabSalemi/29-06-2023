3394d08a9206c1c15506e0773e77e85fb4cbdf8e
GabSalemi <salemi.gabriele94@gmail.com>
2023-06-30 17:44:36 +0200

    feat: checkbox incompleta

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -7,22 +7,22 @@ const Filters = ({productData, setProductData}) => {
     const [checkBelgian, setCheckBelgian] = useState(false);
     const [checkGerman, setCheckGerman] = useState(false);
 
+    let americanProduct = productData.filter((product) => product.ingredients.yeast.includes("American")) 
+    let pilsenProduct = productData.filter((product) => product.ingredients.yeast.includes("Pilsen")) 
+    let belgianProduct = productData.filter((product) => product.ingredients.yeast.includes("Belgian"))
+    let germanProduct = productData.filter((product) => product.ingredients.yeast.includes("German"))
 
-    const onCheckAmerican = () => setCheckAmerican((prev) => !prev)
-
-    const onCheckPilsen = () => setCheckPilsen((prev) => !prev);
-
-    const onCheckBelgian = () => setCheckBelgian((prev) => !prev);
-
-    const onCheckGerman = () => setCheckGerman((prev) => !prev);
-
+    const onlyAmerican = () => setProductData(americanProduct)
+    const onlyPilsen = () => productData(pilsenProduct)
+    const onlyBelgian = () => productData(belgianProduct)
+    const onlyGerman = () => productData(germanProduct)
 
 
     return <div className="filters__div">
-        <input type="checkbox" name="American" checked={checkAmerican} onChange={onCheckAmerican}/>
-        <input type="checkbox" name="Pilsen" checked={checkPilsen} onChange={onCheckPilsen}/>
-        <input type="checkbox" name="Belgian" checked={checkBelgian} onChange={onCheckBelgian}/>
-        <input type="checkbox" name="German" checked={checkGerman} onChange={onCheckGerman}/>
+        <input type="checkbox" name="American" checked={checkAmerican} onChange={onlyAmerican}/>
+        <input type="checkbox" name="Pilsen" checked={checkPilsen} onChange={onlyPilsen}/>
+        <input type="checkbox" name="Belgian" checked={checkBelgian} onChange={onlyBelgian}/>
+        <input type="checkbox" name="German" checked={checkGerman} onChange={onlyGerman}/>
     </div>
 }
 
